755f819309be5da58b59e2206665552a177ee613
Alejandro Ortega <161636827+AlexOrtega301@users.noreply.github.com>
2026-01-13 11:28:40 -0600

    Add Tkinter grid layout example

---
+++ Tkinter/2.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Grid layout")
+
+for i in range(4):
+    for j in range(8):
+        frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j, padx=5, pady=5)
+
+        label = tk.Label(
+            master=frame,
+            text=f"Row {i}\nColumn {j}"
+        )
+        label.pack()
+
+window.mainloop()
