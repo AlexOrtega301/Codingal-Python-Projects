a3bcd9eaa1615324c8b7fb75d70213b5d2d8ef2c
MrRoboto9014 <david.alejandro.ortega301@gmail.com>
2026-05-12 11:29:31 -0600

    Add introductory Python print statements

---
+++ Recap/Welcome to the World of Coding.py
@@ -0,0 +1,15 @@
+#Print a Sentence
+print("Hi! I am coding :D")
+
+#Print a Number
+print(8)
+
+#Next Line after print
+print("Hello World! \n")
+
+#Pass multiple arguments for printing
+print("codingal" \
+      " is cool",5)
+
+#End argument of Print Statement
+print("welcome to ", end= "*")
