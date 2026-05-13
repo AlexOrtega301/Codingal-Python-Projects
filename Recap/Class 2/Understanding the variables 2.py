98c6be7ea7019985b2a4f5a62f0813fe0252a415
MrRoboto9014 <estudiante01913.cfia@labmexia.gob.mx>
2026-05-13 12:19:42 -0600

    Create Understanding the variables 2.py
    Added a Python script demonstrating variable creation and user input.

---
+++ Recap/Class 2/Understanding the variables 2.py
@@ -0,0 +1,15 @@
+# Creating different variables
+a = 10
+b = "Python"
+bool = False
+print(a)
+print(b)
+print(bool)
+
+language = "Roblox Lua"
+print(language)
+
+# User Input
+city = input("Enter your city/alcadia: ")
+
+print("\nHello from", city, "\nWelcome to programming")
