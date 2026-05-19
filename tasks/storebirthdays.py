7454c6a3898c81c329483ce84d74dcd5eb2ed429
MrRoboto9014 <estudiante01913.cfia@labmexia.gob.mx>
2026-05-19 16:19:27 -0600

    Add script to store and print birthday dates

---
+++ tasks/storebirthdays.py
@@ -0,0 +1,14 @@
+# Storing birthday dates in variables
+
+birthday1 = "12 January"
+birthday2 = "28 February"
+birthday3 = "7 March"
+birthday4 = "15 July"
+birthday5 = "30 December"
+
+# Printing the birthday dates
+print(birthday1)
+print(birthday2)
+print(birthday3)
+print(birthday4)
+print(birthday5)
