594f212a92feff3d9b57535249f1e702f2701e46
MrRoboto9014 <estudiante01913.cfia@labmexia.gob.mx>
2026-05-19 16:20:37 -0600

    Add uppercase conversion for congratulations message

---
+++ tasks/uppercasecongrats.py
@@ -0,0 +1,8 @@
+# Storing the message in lowercase
+message = "congratulations"
+
+# Converting the message to uppercase
+uppercase_message = message.upper()
+
+# Printing the result
+print(uppercase_message)
