431e269fcc5422f0dee895d5b6c94d7b507accc7
MrRoboto9014 <estudiante01913.cfia@labmexia.gob.mx>
2026-05-19 11:28:16 -0600

    Add string reversal functionality
    Implement string reversal using slicing.

---
+++ Recap/Class 3/String Operation.py
@@ -0,0 +1,11 @@
+#input a word
+text = str(input("Enter a string: "))
+
+# Reverse String
+# using step value as -1 to iterate in reverse
+revText = text[::-2] # slicing method to reverse the string GASP it now reverse 2 :O
+text = revText #cool variable assignment :D
+
+print("Reverse of Given String is:")
+print(text)
+#D: ti wonk uoy dna gnirts a eb nac gnihtyreve
