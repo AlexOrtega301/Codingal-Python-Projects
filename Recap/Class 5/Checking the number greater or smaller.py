ad40da639476a7bd2eb5248a52788531bfd7bca7
MrRoboto9014 <estudiante01913.cfia@labmexia.gob.mx>
2026-05-27 12:26:09 -0600

    Add number comparison logic in Checking the number script
    Implement number comparison to check if it is greater, less, or equal to 100.

---
+++ Recap/Class 5/Checking the number greater or smaller.py
@@ -0,0 +1,13 @@
+i = int(input("enter a number : ")) 
+# now uses elifs to check the number is greater than, less than or equal to 100
+if (i < 100):
+    print ("i is smaller than 100")
+    print ("i'm in if Block")
+elif (i == 100):
+    print ("i is equal to 100")
+    print("i'm in elif Block")
+else:
+    print ("i is greater than 100")
+    print ("i'm in else Block")
+print ("i'm not in if and not in else Block")
+#i dont know what to add here 
