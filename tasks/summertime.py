c90daa35e3babc92d50a4ffce250d04c4782ebd0
MrRoboto9014 <estudiante01913.cfia@labmexia.gob.mx>
2026-05-29 15:06:27 -0600

    Add summertime.py to suggest clothing based on temperature

---
+++ tasks/summertime.py
@@ -0,0 +1,8 @@
+# Program to check suitable clothes based on temperature
+
+temp = int(input("Enter the temperature: "))
+
+if temp >= 25:
+    print("You can wear light and soft clothes.")
+else:
+    print("Wear a jacket or pullover.")
