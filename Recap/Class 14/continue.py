4a49350b272d35a26f6b1114a9b59d6edd289dbe
MrRoboto9014 <david.alejandro.ortega301@gmail.com>
2026-07-14 11:31:52 -0600

    Add files via upload

---
+++ Recap/Class 14/continue.py
@@ -0,0 +1,10 @@
+var = 10
+
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+        #display result
+    print ('Current value :', var)
+print ("\n The loop is ended")
+
---
+++ Recap/Class 14/pass.py
@@ -0,0 +1,16 @@
+for x in range(10): 
+    if x % 20 == 0: 
+        print("crack")
+
+    elif x % 15 == 0:
+        pass
+
+    elif x % 5 == 0: #
+        print("ASdas")
+
+    elif x % 3 == 0:
+        print("blrz")
+
+    else:
+        print(x)
+
