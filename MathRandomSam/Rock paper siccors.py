4168d325e3a7d8ef3a9cad5f588a3df542e17a08
MrRoboto9014 <david.alejandro.ortega301@gmail.com>
2026-07-21 11:32:43 -0600

    Add files via upload

---
+++ MathRandomSam/Rock paper siccors.py
@@ -0,0 +1,30 @@
+import random
+while True:
+    user_action = input("rock, paper, scissors")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou Choose {user_action}, computer choose {computer_action}.\n")
+
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+    else:
+        print("That is not an option.")
+    play_again = input("Play again? (y/n): ")
+    if play_again != "y":
+        break
